ac79fcbf5cab1badff6ca122b4bed763378e596f
Johnnie Simpson <johnniesimpson@outlook.com>
2020-11-30 23:58:57 -0500

    Added empty arrays to store answers and score. Moved timer position in html. Added timer function on start button click.

--- assets/script.js
+++ assets/script.js
@@ -7,33 +7,43 @@ var jumbo = document.getElementById("myJumbo");
 var startBtn = document.getElementById("startBtn");
 var progBar =  document.getElementById("progBar");
 var main = document.getElementById("main");
+var timer = document.getElementById("timer");
 
 // Set default content
 h1El.textContent = "Coding Quiz";
 jumbo.textContent = "Test your coding knowledge with this multiple choice quiz. You're on the clock!";
-startBtn.textContent = "Let's do this!";
+startBtn.textContent = "Start";
 progBar.style.visibility = "hidden";
+var userAnswers = [];
+var userScore = [];
+var timeRem = 60;
 
-// Question Objects 
-
-var Quest1 = {
-    question: "Question 1: 2 + 2 =",
-    answer1: "1",
-    answer2: "2",
-    answer3: "3",
-    answer4: "4",
-}
+// Questions Object
+var quizQuestions = [
+    Quest1 = {
+        question: "Question 1: 2 + 2 =",
+        answer1: "a: 1",
+        answer2: "b: 2",
+        answer3: "c: 3",
+        answer4: "d: 4",
+    }
+]
 
 // Begin Quiz when Start button is clicked
 function startQuiz() {
     startBtn.style.visibility = "hidden";
     progBar.style.visibility = "visible";
     main.textContent = Quest1.question;
-    for (var i = 0; i < Quest1.length; i++) {
+    function setTime() {
+        var timerInterval = setInterval(function() {
+            timeRem--;
+            timer.textContent = timeRem;
 
+            if(timeRem === 0) {
+                clearInterval(timerInterval);
+                endQuiz();
+            }
+        }, 1000);
     }
-    
-    main.textContent = Quest1.question;
-    main.appendChild
-    
+    setTime();
 }
