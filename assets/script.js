f0b7d7742f68a7edeffe5a94344c8ff8b88e003c
Johnnie Simpson <johnniesimpson@outlook.com>
2020-11-30 22:41:30 -0500

    Testing question display functionality in javascript. Repositioned progress bar at the bottom of the viewport.

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var h1El = document.getElementById("jumboHead");
 var jumbo = document.getElementById("myJumbo");
 var startBtn = document.getElementById("startBtn");
 var progBar =  document.getElementById("progBar");
+var main = document.getElementById("main");
 
 // Set default content
 h1El.textContent = "Coding Quiz";
@@ -13,9 +14,26 @@ jumbo.textContent = "Test your coding knowledge with this multiple choice quiz. 
 startBtn.textContent = "Let's do this!";
 progBar.style.visibility = "hidden";
 
+// Question Objects 
+
+var Quest1 = {
+    question: "Question 1: 2 + 2 =",
+    answer1: "1",
+    answer2: "2",
+    answer3: "3",
+    answer4: "4",
+}
+
 // Begin Quiz when Start button is clicked
 function startQuiz() {
     startBtn.style.visibility = "hidden";
     progBar.style.visibility = "visible";
+    main.textContent = Quest1.question;
+    for (var i = 0; i < Quest1.length; i++) {
+
+    }
+    
+    main.textContent = Quest1.question;
+    main.appendChild
     
 }
