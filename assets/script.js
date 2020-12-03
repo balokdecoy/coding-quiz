abde6cda8f450549e661a9e14db537209e535536
Johnnie Simpson <johnniesimpson@outlook.com>
2020-12-02 22:47:56 -0500

    Update CSS. Modify HTML with new ID tags to adjust bootstrap element formatting. Remove unnecessary code from script.

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,6 @@ var progBar =  document.getElementById("progBar");
 var main = document.getElementById("main");
 var timer = document.getElementById("timer");
 
-// Create elements
-var buttons = document.createElement("button");
-var question = questions
-
 // Set default content
 h1El.textContent = "> Coding Quiz";
 jumbo.textContent = "Test your coding knowledge with this multiple choice quiz. Incorrect answers knock 2 seconds off your time.";
@@ -54,6 +50,7 @@ function startQuiz() {
     main.append(questions[0].question1);
     for (var i = 0; i < questions[0].choices1.length; i++) {
         var buttons = document.createElement("button");
+        buttons.className = "btn btn-info";
         buttons.textContent = questions[0].choices1[i];
         main.append(buttons);
     }
