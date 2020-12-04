fde243a9441e9d0d22b3dde5da31c9d48a3e2c44
Johnnie Simpson <johnniesimpson@outlook.com>
2020-12-04 14:36:26 -0500

    Major refactoring of program logic. Created variables for high score storage, question index for looping through question writes to index file, refactored getQuestion function to be dynamic. Simplified startQuiz function and extracted setTime, endQuiz, and eventListener from startQuiz.

--- assets/script.js
+++ assets/script.js
@@ -22,29 +22,66 @@ var timeRem = 60;
 // Question array with specific question objects
 var questions = [
     {
-        question1: "Question 1: Who invented Linux?",
-        choices1: [
+        question: "Question 1: Who invented Linux?",
+        choices: [
             "Linus Torvalds",
             "Bruce Springsteen",
             "Bill Gates",
             "Steve Jobs",
         ],
-        correct1: "Linus Torvalds",
+        correct: "Linus Torvalds",
     },
     {
-        question2: "Who wrote Moby Dick?",
-        choices2: [
+        question: "Who wrote Moby Dick?",
+        choices: [
             "JK Rowling",
             "Barrack Obama",
             "David Lynch",
             "Herman Melville",
         ],
-        correct2: "Herman Melville",
+        correct: "Herman Melville",
     },
 ]
 
-var correctAnswers = [questions[0].correct1, questions[1].correct2];
-console.log(questions[1]);
+var highScores = {
+    initials: "",
+    score: "",
+};
+
+var questionIndex = 0;
+var correctAnswer = "";
+
+function getQuestion() {
+    var currentQuestion = questions[questionIndex];
+    console.log(currentQuestion.choices);
+    currentQuestion.correct;
+    console.log(currentQuestion.correct);
+    main.textContent = currentQuestion.question;
+    
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
+        
+        // Create list items buttons will render to
+        var li = document.createElement("li");
+        li.setAttribute("data-index", i);
+
+        // Create buttons with text content from questions index
+        var button = document.createElement("button");
+        button.textContent = currentQuestion.choices[i];
+        button.className = "btn btn-info myBtn ";
+
+        // Send button list to HTML
+        li.appendChild(button);
+        main.appendChild(li);
+        
+        //console.log(button.className);
+        console.log(li);
+
+        // Write current correct answer to the correctAnswer variable for testing
+        correctAnswer = currentQuestion.correct;
+    }
+}
+getQuestion();
+console.log(correctAnswer);
 
 // Function that starts quiz logic when user clicks Submit
 function startQuiz() {
@@ -52,59 +89,52 @@ function startQuiz() {
     main.style.visibility = "visible";
     nextBtn.style.visibility = "visible";
     main.innerHTML = questions[0].question1;
-
-    for (var i = 0; i < questions[0].choices1.length; i++) {
-
-        // Create list items buttons will render to
-        var li = document.createElement("li");
-        li.setAttribute("data-index", i);
-
-        // Create buttons with text content from questions index
-        var button = document.createElement("button");
-        button.textContent = questions[0].choices1[i];
-        button.className = "btn btn-info myBtn " + [i];
-
-        // Send button list to HTML
-        li.appendChild(button);
-        main.appendChild(li);
-        //multipleChoice.push(button);
-
-        //console.log(button.className);
-    }
-
-    main.addEventListener("click", function(event) {
-        var element = event.target;
-        console.log(event.target.textContent);
-
-        if (element.matches("button") === true && event.target.textContent === correctAnswers[0]) {
-            alert("Correct");
-            ++userScore;
-            console.log(userScore);
-
-            localStorage.setItem("userScore", JSON.stringify(userScore));
-        }
-        else {
-            if (element.matches("button")) {
-                alert("Incorrect");
-                --userScore;
-                console.log(userScore);
-                localStorage.setItem("userScore", JSON.stringify(userScore));
-            }
-        }
-    })
-
-    // Timer function
-    function setTime() {
-        var timerInterval = setInterval(function() {
-            timeRem--;
-            timer.textContent = timeRem;
-
-            if(timeRem === 0) {
-                clearInterval(timerInterval);
-                timer.style.visibility = "hidden";
-                //endQuiz();
-            }
-        }, 1000);
-    }
+    getQuestion();
     setTime();
+    
 }
+
+// Timer function
+function setTime() {
+    var timerInterval = setInterval(function() {
+        timeRem--;
+        timer.textContent = timeRem;
+
+        if(timeRem === 0) {
+            clearInterval(timerInterval);
+            timer.style.visibility = "hidden";
+            //endQuiz();
+        }
+    }, 1000);
+}
+
+function endQuiz() {
+
+}
+
+
+// 
+main.addEventListener("click", function(event) {
+    var element = event.target;
+    console.log(event.target.textContent);
+
+    if (element.matches("button") === true && event.target.textContent === correctAnswer) {
+        alert("Correct");
+        ++userScore;
+        console.log(userScore);
+
+        localStorage.setItem("userScore", JSON.stringify(userScore));
+        questionIndex++;
+        getQuestion();
+    }
+    else {
+        if (element.matches("button")) {
+            alert("Incorrect");
+            --userScore;
+            console.log(userScore);
+            localStorage.setItem("userScore", JSON.stringify(userScore));
+            questionIndex++;
+            getQuestion();
+        }
+    }
+})
